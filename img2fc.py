9e5ba4d3d8323f2d2f76a2d2099e2f85a0622016
Дмитрий Холостов <dkh@ro.ru>
2020-12-31 18:29:54 +0300

    2.0: x30 speed, -95% code

--- img2fc.py
+++ img2fc.py
@@ -3,6 +3,7 @@ import numpy as np
 
 img = cv2.imread(input('pic: '), 0)
 maxScale = int(input('max size: '))
+pixScale = .3
 
 width = np.shape(img)[1]
 height = np.shape(img)[0]
@@ -15,12 +16,21 @@ height = int(height / k)
 img = cv2.resize(img, (width, height))
 
 out = open('img2fc.txt', 'w')
-out.write('сдвиг-влево {}\n'.format(width/2*0.3))
-out.write('сдвиг-вверх {}\n'.format(height/2*0.3))
+out.write('сдвиг-влево {}\n'.format(width/2*pixScale))
+out.write('сдвиг-вверх {}\n'.format(height/2*pixScale))
+
 for y in range(height):
+  lastState = round(img[y][0]/256)
+  lineLength = 1
   for x in range(width):
-    if img[y][x] < 128: out.write('вправо 0.3\n')
-    else: out.write('сдвиг-вправо 0.3\n')
-  out.write('сдвиг-вниз 0.3\n')
-  out.write('сдвиг-влево {}\n'.format(width * 0.3))
+    if round(img[y][x]/256) != lastState or x >= width-1:
+      if not lastState: out.write('вправо {}\n'.format(pixScale * lineLength))
+      else: out.write('сдвиг-вправо {}\n'.format(pixScale * lineLength))
+      lineLength = 1
+      lastState = round(img[y][x]/256)
+    else:
+      lineLength += 1
+      
+  out.write('сдвиг-вниз {}\n'.format(pixScale))
+  out.write('сдвиг-влево {}\n'.format(width * pixScale))
 out.close()
